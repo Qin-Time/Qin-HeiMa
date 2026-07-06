be52020222533a114ae810fd7c7b15d7b692e5c6
Qin-Time <71476146@qq.com>
2026-07-06 10:46:16 +0800

    二级分类功能完成

--- vue3-rabbit/src/router/index.ts
+++ vue3-rabbit/src/router/index.ts
@@ -3,6 +3,7 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 
 const router = createRouter({
@@ -19,6 +20,10 @@ const router = createRouter({
         {
           path: 'category/:id',
           component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
         }
       ]
     },
@@ -27,6 +32,12 @@ const router = createRouter({
       component: Login
     }
   ],
+
+  // 路由滚动行为定制
+  scrollBehavior() {
+    return { top: 0 }
+  }
+
 })
 
 export default router
