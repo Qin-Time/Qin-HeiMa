45f41a13a0955e5056b77e7f554a899ff331d801
Qin-Time <71476146@qq.com>
2026-07-15 15:15:02 +0800

    补充TS声明代码并构建项目

--- vue3-rabbit/env.d.ts
+++ vue3-rabbit/env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+// 为你的 .js 插件文件添加类型声明（可选但推荐）
+declare module '@/directives' {
+  export const lazyPlugin: any
+}
+
+declare module '@/components' {
+  export const componentPlugin: any
+}
--- vue3-rabbit/src/main.ts
+++ vue3-rabbit/src/main.ts
@@ -3,7 +3,7 @@ import '@/styles/common.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import router from './router'
+import router from './router/index.ts'
 import App from './App.vue'
 
 // 引入懒加载指令插件
